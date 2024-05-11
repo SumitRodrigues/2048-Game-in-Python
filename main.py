157f473bb8703431680475f8a9e364627bfd688d
SumitRodrigues <sumitrod11@gmail.com>
2024-05-11 12:09:04 -0700

    Latest update

--- main.py
+++ main.py
@@ -66,7 +66,9 @@ class Tile:
         pass
 
     def move(self, delta):
-        pass
+        self.x += delta[0]
+        self.y += delta[1]
+
 
 def draw_grid(window):
     for row in range(1, ROWS):
@@ -101,6 +103,61 @@ def get_random_pos(tiles):
             break
 
     return row, col
+
+def func(x):
+    return x.col
+
+def move_tiles(window, tiles, clock, direction):
+    updated = True
+    blocks = set()
+
+    if direction == "left":
+        sort_func = lambda x: x.col
+        reverse = False
+        delta = (-MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col - 1}")
+        merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VEL
+        move_check = (
+            lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
+        )
+        ceil = True
+
+
+    elif direction == "right":
+        pass
+    elif direction == "up":
+        pass
+    elif direction == "down":
+        pass
+
+    while updated:
+        clock.tick(FPS)
+        updated = False
+        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse=reverse)
+
+        for i, tile in enumerate(sorted_tiles):
+            if boundary_check(tile):
+                continue
+
+            next_tile = get_next_tile(tile)
+            if not next_tile:
+                tile.move(delta)
+            elif (
+                tile.value == next_tile.value 
+                and tile not in blocks 
+                and next_tile not in blocks
+            ):
+                if merge_check(tile, next_tile):
+                    tile.move(delta)
+                else:
+                    next_tile.value += 2
+                    sorted_tiles.pop(i)
+                    blocks.add(next_tile)
+            elif move_check(tile, next_tile):
+                tile.move(delta)
+            else:
+                continue
 
 def generate_tiles():
     tiles = {}
